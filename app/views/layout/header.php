3513e088c21fbef40f2021201d22ba4f2d7bbce7
anasouzapaiva-pixel <anasouza.paiva@icloud.com>
2026-02-18 10:59:04 -0300

    meucss

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -1,8 +1,12 @@
 <link rel="stylesheet" href="assets/css/style.css">
-<h1 class = "titulo">Copa do mundo</h1>
+<h1 class="titulo">Copa do mundo</h1>
 <hr>
-<a href="?controller=selecao&action=listar">Seleções</a> |
-<a href="?controller=jogo&action=listar">Jogos</a> |
-<a href="?controller=grupo&action=listar">Grupos</a> |
-<a href="?controller=usuario&action=listar">Usuários</a>
+<div class="nav-header">
+    <nav >
+        <a href="?controller=selecao&action=listar">Seleções</a> |
+        <a href="?controller=jogo&action=listar">Jogos</a> |
+        <a href="?controller=grupo&action=listar">Grupos</a> |
+        <a href="?controller=usuario&action=listar">Usuários</a>
+    </nav>
+</div>
 <hr>
